e6057f7b09cdc6069959ab128b6a9504e3983703
LiFangCoding <fanglihust@gmail.com>
2018-09-16 22:29:36 +0900

    Reformat code

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,4 +1,3 @@
-// TODO: Make sure to make this class a part of the synthesizer package
 package synthesizer;
 
 import synthesizer.BoundedQueue;
@@ -17,14 +16,13 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this divsion operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
         int capacity = (int) Math.round(SR / frequency);
-        buffer= new ArrayRingBuffer<>(capacity);
+        buffer = new ArrayRingBuffer<>(capacity);
         for (int i = 0; i < capacity; i++) {
-            buffer.enqueue((double)0);
+            buffer.enqueue((double) 0);
         }
 
     }
@@ -32,7 +30,6 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
         //
@@ -50,7 +47,6 @@ public class GuitarString {
      * the Karplus-Strong algorithm. 
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
         double front = buffer.dequeue();
@@ -60,7 +56,6 @@ public class GuitarString {
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.peek();
     }
 }
